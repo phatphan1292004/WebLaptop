65f25a86b0472a818684906a015a5661dcea493b
Phat <22130206@st.hcmuaf.edu.vn>
2024-12-23 01:02:09 +0700

    khanh ngu

---
+++ WebLaptopNe/Controllers/AdminController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebLaptopNe.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult DashboardAdmin()
+        {
+            return View();
+        }
+
+        public ActionResult ProductAdmin()
+        {
+            return View();
+        }
+        public ActionResult OrderAdmin()
+        {
+            return View();
+        }
+
+        public ActionResult CusAdmin()
+        {
+            return View();
+        }
+        public ActionResult PromotionAdmin()
+        {
+            return View();
+        }
+
+        public ActionResult AccountAdmin()
+        {
+            return View();
+        }
+
+        public ActionResult RevenueAdmin()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebLaptopNe/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebLaptopNe.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductDetail()
+        {
+            return View();
+        }
+
+        public ActionResult Category()
+        {
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult User()
+        {
+            return View();
+        }
+    }
+}
