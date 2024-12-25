4cd70a60d2dd4b08e40f398ca20b377c92364e26
Phat <22130206@st.hcmuaf.edu.vn>
2024-12-25 11:34:48 +0700

    them entity framework

--- WebLaptopNe/Controllers/HomeController.cs
+++ WebLaptopNe/Controllers/HomeController.cs
@@ -1,22 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebLaptopNe.Models;
 
 namespace WebLaptopNe.Controllers
 {
     public class HomeController : Controller
     {
+        WebPPEntities db = new WebPPEntities();
+
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var productList = db.products.ToList();
+            var productImages = db.product_images.ToList();
+          
+
+
+            ViewBag.listSPHome = productList;
+            ViewBag.productImages = productImages;
+
+            return View(productList);
         }
 
-        public ActionResult ProductDetail()
+
+        public ActionResult ProductDetail(int id)
         {
-            return View();
+            var productDetail = db.product_detail.FirstOrDefault(p => p.product_id == id);
+            // Lấy hình ảnh chính
+            var mainImage = db.product_images.FirstOrDefault(pi => pi.product_id == id && pi.main_image);
+
+            // Truyền dữ liệu sang View
+            ViewBag.MainImage = mainImage?.url; // Truyền URL của hình chính
+
+            return View(productDetail);
         }
 
         public ActionResult Category()
